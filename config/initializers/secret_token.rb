f46dfa90ccc8ead8b3785b8be57069c1f526fd1a
Andres <andres@softlutions.es>
2012-11-17 15:17:39 +0100

    MallorcaRoR first meetup.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MallorcaRoR::Application.config.secret_token = '7e450be1ae0f4eb8d5e41fbac099257f2c318618214def2f9a98fb6f3c9e14bb656cb84a552660fccf7af7ca3fe85165eb87128bed8db2ca544a5507f54f4d3b'
